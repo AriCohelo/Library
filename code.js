fd63b8c599b34f8dce925764aff21620f9b38266
AriCohelo <ariel.cohelo@gmail.com>
2023-08-23 02:13:08 +0200

    todo solucionado pero quiero buscar alternativas mas prolijas asi q creare otro branch

--- code.js
+++ code.js
@@ -1,4 +1,4 @@
-const myLibrary = [{
+let myLibrary = [{
     title: 'bestiario',
     author: 'julio cortazar',
     pages: 200,
@@ -60,16 +60,34 @@ function bookInfo() {
 function addBookToTheLibrary(title, author, pages, status) {
     let newBook = new Book(title, author, pages, status)
     myLibrary.push(newBook)
+
+}
+
+function deleteBook(event) {
+    // The event.target represents the element that triggered the event (in this case, the Delete button)
+    let deleteButton = event.target;
+
+    // Use closest() to find the closest ancestor element with the class "bookCard"
+    let bookCard = deleteButton.closest('.bookCard');
+
+    if (bookCard) {
+        // Now you have a reference to the parent book card element, and you can manipulate or remove it as needed
+        bookCard.remove();
+    }
 }
 
 
-
-
-function bookOnBookCard() {
+function bookOnCard() {
+    bookCardsContainer.innerHTML = '';
     for (let i = 0; i < myLibrary.length; i++) {
         let bookCard = document.createElement('div');
         bookCard.classList.add('bookCard');
         bookCardsContainer.appendChild(bookCard);
+        // bookCard.setAttribute('data-index', i);
+
+        // //borrar
+        // let index = bookCard.getAttribute('data-index');
+        // console.log(index);
 
         let cover = document.createElement('img')
         cover.classList.add('cover');
@@ -83,11 +101,13 @@ function bookOnBookCard() {
         infoParag.innerHTML = bookInfo;
         bookCard.appendChild(infoParag);
 
+        let deleteButton = document.createElement('button');
+        deleteButton.innerText = '-'
+        bookCard.appendChild(deleteButton)
+        deleteButton.addEventListener('click', deleteBook)
     }
-
 }
-addBookToTheLibrary('un libro', 'de alguien', 100000, 'nose')
-bookOnBookCard();
+
 
 let addBookButton = document.getElementById('addBookButton');
 let modal = document.getElementById('modal')
@@ -98,8 +118,23 @@ addBookButton.addEventListener('click', () => {
 let submitButton = document.getElementById('submitButton');
 submitButton.addEventListener('click', submit)
 
-function submit() {
+function submit(event) {
     event.preventDefault();
-    let title = document.getElementById('title')
-    console.log(title.value);
+    let status = '';
+    let title = document.getElementById('title');
+    let author = document.getElementById('author');
+    let pages = document.getElementById('pages');
+    let readed = document.getElementById('readed');
+    let notReaded = document.getElementById('notReaded')
+    readed.checked ? status = 'readed' : status = 'not readed';
+    addBookToTheLibrary(title.value, author.value, pages.value, status);
+    bookOnCard();
+    modal.close();
+    title.value = '';
+    author.value = '';
+    pages.value = '';
+    notReaded.checked = true;
+
 }
+
+bookOnCard();
